c6d60d8b82d3fd69bcd8cdbd83fa6969daacaaa5
Samuel Curran <samuel.curran@fms-ea.com>
2026-09-15 11:48:55 +0200

    feat: add __str__ method to Novel

--- goncourt/models/novel.py
+++ goncourt/models/novel.py
@@ -19,3 +19,22 @@ class Novel:
     publisher_price: Decimal
     author: Optional[Person] = field(default=None, init=False)
     main_character: list[MainCharacter] = field(default_factory=list, init=False)
+
+    def __str__(self) -> str:
+        result = f"Titre : {self.title}\n"
+        result += f"Auteur : {self.author.firstname} {self.author.lastname}\n" if self.author else "Auteur : Inconnu\n"
+        result += f"Éditeur : {self.editor}\n"
+        result += f"Date de publication : {self.publication_date.strftime('%d/%m/%Y')}\n"
+        result += f"Nombre de pages : {self.number_of_pages}\n"
+        result += f"ISBN : {self.ISBN}\n"
+        result += f"Prix éditeur : {self.publisher_price:.2f} €\n"
+        result += f"Résumé : {self.summary}\n"
+
+        if self.main_character:
+            result += "Personnages principaux :\n"
+            for character in self.main_character:
+                result += f"  - {character.name}\n"
+        else:
+            result += "Personnages principaux : Aucun\n"
+
+        return result
