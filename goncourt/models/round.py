156fa09d73901595fe50fb8d1e00e1ec84f4de78
Samuel Curran <samuel.curran@fms-ea.com>
2026-09-15 15:45:47 +0200

    refactor: remove circular unused attributes from Round

--- goncourt/models/round.py
+++ goncourt/models/round.py
@@ -2,11 +2,8 @@ from dataclasses import field, dataclass
 from datetime import date
 from typing import Optional
 
-from models.novel import Novel
-
 @dataclass
 class Round:
     id_round: Optional[int] = field(default=None, init=False)
     number: int
     date_round: date
-    ketps_novel: list[Novel] = field(default_factory=list, init=False)
