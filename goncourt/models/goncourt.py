979da03f1d7582bdcb472265992dec541888c39d
Samuel Curran <samuel.curran@fms-ea.com>
2026-09-15 15:45:07 +0200

    refactor: remove circular attributes from Goncourt unused

--- goncourt/models/goncourt.py
+++ goncourt/models/goncourt.py
@@ -1,13 +1,9 @@
 from dataclasses import dataclass, field
 from typing import List
 
-from models.round import Round
 from models.vote import Vote
 
 @dataclass
 class Goncourt:
     year: int
-    rounds: list[Round] = field(default_factory=list, init=False)
-    def get_rounds(self) -> List[Round]:
-        return self.rounds
     votes: list[Vote] = field(default_factory=list, init=False)
