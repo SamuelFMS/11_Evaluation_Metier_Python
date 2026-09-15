c3bf0c0449f3fa041cd3e19c3af14e35536478b6
Samuel Curran <samuel.curran@fms-ea.com>
2026-09-15 13:33:01 +0200

    feat: add round business and dao

---
+++ goncourt/business/round_business.py
@@ -0,0 +1,12 @@
+from business.business import Business
+from dao import round_dao, goncourt_dao
+from dao.goncourt_dao import GoncourtDao
+from dao.round_dao import RoundDao
+from models.round import Round
+
+class RoundBusiness(Business):
+    round_dao: RoundDao = RoundDao()
+    goncourt_dao: GoncourtDao = GoncourtDao()
+
+    def get_round_for_year(self, year: int) -> list[Round]:
+        return self.round_dao.get_all_by_related_id(self.connection.cursor(), self.goncourt_dao, year)
---
+++ goncourt/dao/round_dao.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from dao.dao import Dao
+from models.round import Round
+
+@dataclass
+class RoundDao(Dao[Round]):
+    @classmethod
+    def get_table_name(cls):
+        return "round"
+
+    def map_record(self, record) -> Optional[Round]:
+        _round: Optional[Round] = None
+        if record is not None:
+            _round = Round(number=record["number"], date_round=record["date_"])
+            _round.id_round = record["id_round"]
+        return _round
