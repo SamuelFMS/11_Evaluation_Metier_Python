0c6c1d95b8bfc0e7507d97583828850835e38e4e
Samuel Curran <samuel.curran@fms-ea.com>
2026-09-16 08:33:33 +0200

    feat: allow switching between user and president views

--- goncourt/main.py
+++ goncourt/main.py
@@ -1,3 +1,5 @@
+from utils import input_utils
+from views.president_view import PresidentView
 from views.user_view import UserView
 
 # Press Maj+F10 to execute it or replace it with your code.
@@ -8,6 +10,12 @@ from views.user_view import UserView
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    UserView.display()
+    print("1- Souhaitez vous etre president")
+    print("2- Souhaitez vous etre visiteur")
+    choice: int = input_utils.input_number("Votre choix: ",1,2)
+    if choice == 1:
+        PresidentView.display()
+    elif choice == 2:
+        UserView.display()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
